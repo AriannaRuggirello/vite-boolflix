8d5717cb560889ccc53b24f71bc81ade17eece07
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-06-07 13:56:21 +0200

    create base command

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { string } from 'i/lib/util'
+import { reactive } from 'vue'
+
+export const store = reactive({
+    // url della chiamata api
+    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=ea01ac0b06b542fd7860fe34a3b02f03&query=ritorno+al+futuro',
+
+})
